e8cfccf99b099c509dae882ccfb7819e18bdb1b4
Evan Quinlan <evan.quinlan@gmail.com>
2019-05-22 15:22:33 -0400

    Create output program.

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+# Aetherspeak lexicon
 Lexicon = {
     "particles" => ["a", "anu", "e", "en", "kin", "la", "li", "mu", "nanpa", "o", "pi", "seme", "taso"],
     "verbs" => ["akesi", "alasa", "ali", "apeja", "esun", "ijo", "ilo", "insa", "jan", "jo", "kala", "kalama", "kasi", "kepeken", "kili", "kiwen", "ko", "kon", "kulupu", "kute", "lawa", "leko", "len", "linja", "lipu", "lon", "luka", "lupa", "ma", "mama", "mani", "meli", "mi", "mije", "moku", "monsi", "monsuta", "mun", "mute", "nanpa", "nasin", "nena", "nimi", "noka", "oko", "olin", "ona", "open", "pali", "palisa", "pan", "pana", "pata", "pilin", "pipi", "poka", "poki", "selo", "sewi", "sijelo", "sike", "sinpin", "sitelen", "sona", "soweli", "suno", "supa", "tawa", "telo", "tenpo", "toki", "tomo", "unpa", "uta", "utala", "waso"],
@@ -8,6 +9,7 @@ Lexicon = {
     "prepositions" => ["tawa", "sama", "tan", "kepeken", "lon"]
 }
 
+# Context-free grammar definition
 Variables = [:adjective, :adjectives, :but, :fives, :hundreds, :of, :noun, :noun_except_i, :ones, :ordinal, :pre_verb, :precondition, :predicate, :preposition, :prepositional, :sentence, :sentences, :separator, :spell, :still, :subject, :subjects, :time, :twenties, :twos, :verb, :direct_object, :word, :words, :yield]
 Terminals = [Lexicon.values.flatten.uniq, "!", ".", ":"]
 Rules = {
@@ -43,3 +45,41 @@ Rules = {
     yield: [nil, ["mu", :words]]
 }
 Start = :spell
+
+# Parameters
+num_spells = 100
+
+# Execution
+def produce from_variable
+    # Initialize output
+    to_str = []
+
+    # Process symbol
+    if from_variable.is_a?(Symbol)
+        # Choose random rule
+        rule = [Rules[from_variable].sample].flatten
+        
+        # Produce from rule
+        variables = rule.map { |variable| produce(variable) }
+
+        # Push to output
+        to_str.push variables
+    elsif from_variable.is_a?(String)
+        # Push strings to output
+        to_str.push from_variable
+    end
+
+    # Output
+    to_str.flatten
+end
+
+for i in 1..num_spells
+    # Join terminals
+    terminals = produce(Start).join(" ");
+
+    # Remove spaces before punctuation
+    terminals_formatted = terminals.gsub(/\s[.:!]/) { |terminal| terminal.strip }
+
+    # Print output
+    print terminals_formatted + "\n"
+end
